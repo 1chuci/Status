04ddc9ee8e477039b28b88ccb00751ccadc60051
楚辞 <115083331+1chuci@users.noreply.github.com>
2023-07-12 20:08:51 +0800

    Add files via upload

---
+++ build/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'IMSYY 站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2215355-821fd37f21f220c5d1942731',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://www.raindyhome.top/'
+    },
+    {
+      text: '博客',
+      url: 'https://www.raindy.xyz/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/1chuci/'
+    },
+  ],
+};
